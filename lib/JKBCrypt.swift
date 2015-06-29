02370eeb8f71e28b872953895fad84f555aa6567
Joe Kramer <meanjoe45@users.noreply.github.com>
2015-06-29 16:32:50 -0400

    Added license information to code files

--- lib/JKBCrypt.swift
+++ lib/JKBCrypt.swift
@@ -2,8 +2,46 @@
 //  JKBCrypt.swift
 //  JKBCrypt
 //
-//  Created by Joe Kramer on 6/19/15.
+//  Created by Joe Kramer on 6/19/2015.
 //  Copyright (c) 2015 Joe Kramer. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// ----------------------------------------------------------------------
+//
+// This Swift port is based on the Objective-C port by Jay Fuerstenberg.
+// https://github.com/jayfuerstenberg/JFCommon
+//
+// ----------------------------------------------------------------------
+//
+// The Objective-C port is based on the original Java implementation by Damien Miller
+// found here: http://www.mindrot.org/projects/jBCrypt/
+// In accordance with the Damien Miller's request, his original copyright covering
+// his Java implementation is included here:
+//
+// Copyright (c) 2006 Damien Miller <djm@mindrot.org>
+//
+// Permission to use, copy, modify, and distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 //
 
 import Foundation
